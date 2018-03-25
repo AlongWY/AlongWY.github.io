bd50c747360dac89f29c11302d03e5dc08eb044c
alongwy <15939423778@163.com>
2018-03-25 15:23:38 +0800

    主页配置修正

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,33 +6,30 @@
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
-    data() {
-      return {
-        msg: '欢迎光临寒舍'
-      }
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '欢迎光临寒舍'
     }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
