50a6a302a9b947bfc806c5dd15f93325e4921a60
AlongWY <AlongWY@users.noreply.github.com>
2021-08-30 06:51:58 +0000

    deploy: AlongWY/website@dd2af4266f2a86edf46740a656d464bdf595d3be

--- js/search.js
+++ js/search.js
@@ -1,108 +1,157 @@
+window.addEventListener("DOMContentLoaded", function(event)
+{
+  var index = null;
+  var lookup = null;
+  var queuedTerm = null;
 
-summaryInclude=60;
-var fuseOptions = {
-  shouldSort: true,
-  includeMatches: true,
-  threshold: 0.0,
-  tokenize:true,
-  location: 0,
-  distance: 100,
-  maxPatternLength: 32,
-  minMatchCharLength: 3,
-  keys: [
-    {name:"title",weight:0.8},
-    {name:"contents",weight:0.5},
-    {name:"tags",weight:0.3}
-  ]
-};
+  var form = document.getElementById("search");
+  var input = document.getElementById("search-input");
 
+  form.addEventListener("submit", function(event)
+  {
+    event.preventDefault();
 
-var searchQuery = param("s");
-if(searchQuery){
-  $("#search-query").val(searchQuery);
-  executeSearch(searchQuery);
-}else {
-  $('#search-results').append("<p>Please enter a word or phrase above</p>");
-}
+    var term = input.value.trim();
+    if (!term)
+      return;
 
+    startSearch(term);
+  }, false);
 
+  function startSearch(term)
+  {
+    // Start icon animation.
+    form.setAttribute("data-running", "true");
 
-function executeSearch(searchQuery){
-  $.getJSON( "/index.json", function( data ) {
-    var pages = data;
-    var fuse = new Fuse(pages, fuseOptions);
-    var result = fuse.search(searchQuery);
-    console.log({"matches":result});
-    if(result.length > 0){
-      populateResults(result);
-    }else{
-      $('#search-results').append("<p>No matches found</p>");
+    if (index)
+    {
+      // Index already present, search directly.
+      search(term);
     }
-  });
-}
+    else if (queuedTerm)
+    {
+      // Index is being loaded, replace the term we want to search for.
+      queuedTerm = term;
+    }
+    else
+    {
+      // Start loading index, perform the search when done.
+      queuedTerm = term;
+      initIndex();
+    }
+  }
 
-function populateResults(result){
-  $.each(result,function(key,value){
-    var contents= value.item.contents;
-    var snippet = "";
-    var snippetHighlights=[];
-    var tags =[];
-    if( fuseOptions.tokenize ){
-      snippetHighlights.push(searchQuery);
-    }else{
-      $.each(value.matches,function(matchKey,mvalue){
-        if(mvalue.key == "tags"){
-          snippetHighlights.push(mvalue.value);
-        }else if(mvalue.key == "contents"){
-          start = mvalue.indices[0][0]-summaryInclude>0?mvalue.indices[0][0]-summaryInclude:0;
-          end = mvalue.indices[0][1]+summaryInclude<contents.length?mvalue.indices[0][1]+summaryInclude:contents.length;
-          snippet += contents.substring(start,end);
-          snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0],mvalue.indices[0][1]-mvalue.indices[0][0]+1));
+  function searchDone()
+  {
+    // Stop icon animation.
+    form.removeAttribute("data-running");
+
+    queuedTerm = null;
+  }
+
+  function initIndex()
+  {
+    var request = new XMLHttpRequest();
+    request.open("GET", "/index.json");
+    request.responseType = "json";
+    request.addEventListener("load", function(event)
+    {
+      lookup = {};
+      index = lunr(function()
+      {
+        // Uncomment the following line and replace de by the right language
+        // code to use a lunr language pack.
+
+        // this.use(lunr.de);
+
+        this.ref("permalink");
+
+        // If you added more searchable fields to the search index, list them here.
+        this.field("title");
+        this.field("content");
+        this.field("description");
+
+        for (var doc of request.response)
+        {
+          this.add(doc);
+          lookup[doc.permalink] = doc;
         }
       });
-    }
 
-    if(snippet.length<1){
-      snippet += contents.substring(0,summaryInclude*2);
-    }
-    //pull template from hugo templarte definition
-    var templateDefinition = $('#search-result-template').html();
-    //replace values
-    var output = render(templateDefinition,{key:key,title:value.item.title,link:value.item.permalink,tags:value.item.tags,categories:value.item.categories,snippet:snippet});
-    $('#search-results').append(output);
-
-    $.each(snippetHighlights,function(snipkey,snipvalue){
-      $("#summary-"+key).mark(snipvalue);
-    });
-
-  });
-}
-
-function param(name) {
-    return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ');
-}
-
-function render(templateString, data) {
-  var conditionalMatches,conditionalPattern,copy;
-  conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
-  //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
-  copy = templateString;
-  while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
-    if(data[conditionalMatches[1]]){
-      //valid key, remove conditionals, leave contents.
-      copy = copy.replace(conditionalMatches[0],conditionalMatches[2]);
-    }else{
-      //not valid, remove entire section
-      copy = copy.replace(conditionalMatches[0],'');
-    }
+      // Search index is ready, perform the search now
+      search(queuedTerm);
+    }, false);
+    request.addEventListener("error", searchDone, false);
+    request.send(null);
   }
-  templateString = copy;
-  //now any conditionals removed we can do simple substitution
-  var key, find, re;
-  for (key in data) {
-    find = '\\$\\{\\s*' + key + '\\s*\\}';
-    re = new RegExp(find, 'g');
-    templateString = templateString.replace(re, data[key]);
+
+  function search(term)
+  {
+    var results = index.search(term);
+
+    // The element where search results should be displayed, adjust as needed.
+    var target = document.getElementById("search-results");
+
+    while (target.firstChild)
+      target.removeChild(target.firstChild);
+
+    var title = document.createElement("h3");
+    title.id = "search-results";
+    title.className = "list-title";
+
+    if (results.length == 0)
+      title.textContent = `No results found for “${term}”`;
+    else if (results.length == 1)
+      title.textContent = `Found one result for “${term}”`;
+    else
+      title.textContent = `Found ${results.length} results for “${term}”`;
+    target.appendChild(title);
+    document.title = title.textContent;
+
+    var template = document.getElementById("search-result");
+    for (var result of results)
+    {
+      var doc = lookup[result.ref];
+
+      // Fill out search result template, adjust as needed.
+      var element = template.content.cloneNode(true);
+      element.querySelector(".summary-title-link").href =
+          element.querySelector(".read-more-link").href = doc.permalink;
+      element.querySelector(".summary-title-link").textContent = doc.title;
+      element.querySelector(".summary").innerHTML = doc.description;
+      target.appendChild(element);
+    }
+    title.scrollIntoView(true);
+
+    searchDone();
   }
-  return templateString;
-}
+
+  // This matches Hugo's own summary logic:
+  // https://github.com/gohugoio/hugo/blob/b5f39d23b8/helpers/content.go#L543
+  function truncate(text, minWords)
+  {
+    var match;
+    var result = "";
+    var wordCount = 0;
+    var regexp = /(\S+)(\s*)/g;
+    while (match = regexp.exec(text))
+    {
+      wordCount++;
+      if (wordCount <= minWords)
+        result += match[0];
+      else
+      {
+        var char1 = match[1][match[1].length - 1];
+        var char2 = match[2][0];
+        if (/[.?!"]/.test(char1) || char2 == "\n")
+        {
+          result += match[1];
+          break;
+        }
+        else
+          result += match[0];
+      }
+    }
+    return result;
+  }
+}, false);
